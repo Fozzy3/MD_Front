d230da1a82f6bd8f6c79942494416c4da4404aec
Fozzy3 <carlmora027@gmail.com>
2024-01-12 20:45:10 -0500

    update docker

--- src/environments/enviorment.ts
+++ src/environments/enviorment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  // API_URL: 'http://localhost:8080/moduloweb',
-  API_URL: 'http://localhost:8090',
+  API_URL: 'http://localhost:8080',
+  // API_URL: 'http://localhost:8090',
   // API_URL: '192.168.1.37:8080',
 }
 
-// importand command for prod:  ng build --configuration production --base-href="/moduloweb/"
+// importad command for prod:  ng build --configuration production --base-href="/moduloWeb-3.2.1/"
